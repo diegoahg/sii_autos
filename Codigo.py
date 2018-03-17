bf8bc13ae13b0b661ed725d89f550d1a00bf960d
R0bert01de <r.i.o.p@live.cl>
2018-03-17 19:38:34 -0300

    master - Coneccion a bd local

--- Codigo.py
+++ Codigo.py
@@ -1,4 +1,10 @@
 import csv
+import MySQLdb
+
+db = MySQLdb.connect(host="localhost",    # tu host, usualmente localhost
+                     user="r0bert01de",   # tu usuario
+                     passwd="1709",       # tu password
+                     db="SII_2018")       # el nombre de la base de datos
 
 with open('liv2018xls.csv') as File:  
     reader = csv.reader(File)
