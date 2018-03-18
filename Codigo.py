6f9e28ecd0231411f70592d8ef36625506f0bb0d
R0bert01de <r.i.o.p@live.cl>
2018-03-17 23:08:02 -0300

    master - primeros pasos para llenar la bd, aun por terminar

--- Codigo.py
+++ Codigo.py
@@ -5,10 +5,35 @@ db = MySQLdb.connect(host="localhost",    # tu host, usualmente localhost
                      user="r0bert01de",   # tu usuario
                      passwd="1709",       # tu password
                      db="SII_2018")       # el nombre de la base de datos
-
+cursor = db.cursor()
+mi_query = "SELECT * FROM IDENTIFICACION_DE_VEHICULO"
+cursor.execute(mi_query)
+cursor.fetchall()
+i = 0
+print('hdp')
 with open('liv2018xls.csv') as File:  
     reader = csv.reader(File)
     for row in reader:
     	particion = str(row[0]).split(";")
-    print(particion)
-    
+		vehiculoID = int(i)
+		codigo = particion[0]
+		ano = int(particion[1])
+		tipo = particion[2]
+		marca = particion[3]
+		modelo_raw = particion[4]
+		modelo = str(0)
+		version = str(0)
+		ptas = int(particion[5])
+		cilindr = int(particion[6])
+		comb = particion[7]
+		trans = particion[8]
+		equip = particion[9]
+		tas_2018 = int(particion[10])
+		perm_2018 = int(particion[11])
+		i = i +1
+		sql = "INSERT INTO IDENTIFICACION_DE_VEHICULO (vehiculoID,codigo,ano,tipo,marca,modelo_raw,modelo,version,ptas,cilindr,comb,trans,equip,tas_2018,perm_2018) VALUES (%d, %s, %d, %s, %s, %s, %s, %s, %d, %d,  %s, %s, %s, %d, %d)"
+		print(sql)
+		registro = (vehiculoID,codigo,ano,tipo,marca,modelo_raw,modelo,version,ptas,cilindr,comb,trans,equip,tas_2018,perm_2018)
+		print(registro)
+		#cursor.execute(sql,registro)
+		#cursor.commit()
